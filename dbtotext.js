e2fa78cc329764b25dff045e519b8d848cb5f9b0
Dave <dave.turmawan@outlook.com>
2024-11-30 08:00:02 +0700

    let's go

--- dbtotext.js
+++ dbtotext.js
@@ -2,7 +2,7 @@ import { Database } from "bun:sqlite";
 import fs from "fs";
 const db = new Database("database.db");
 const target = "output/output.txt";
-const list = db.query(`SELECT * FROM sites WHERE currentlyBeingModified = 0 AND screenshotPath NOT NULL AND screenshottedUrl = "https://heylink.me/deactivated/"`).all();
+const list = db.query(`SELECT * FROM sites WHERE currentlyBeingModified = 0 AND screenshotPath NOT NULL AND screenshottedUrl != "https://heylink.me/deactivated/"`).all();
 const seenUrls = new Set(); // Prevent duplicates
 for (const site of list) {
     if (!seenUrls.has(site.url)) {
